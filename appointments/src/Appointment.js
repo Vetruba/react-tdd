c6a3a9c89e025819d23d1f275bbef90de2cea8a7
Chase Vetruba <Vetruba00@gmail.com>
2024-03-22 17:10:44 -0500

    starting Selecting data to veiw

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,2 +1,18 @@
 import React from 'react';
-export const Appointment = () => <div>Ashley</div>;
+const appointmentTimeOfDay = startsAt => {
+    const [h, m] = new Date(startsAt).toTimeString().split(':');
+    return `${h}:${m}`;
+}
+export const Appointment = ({ customer }) => (
+    <div>{customer.firstName}</div>
+);
+export const AppointmentsDayView = ({ appointments }) =>
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map(appointment => (
+                <li key={appointment.startsAt}>
+                    {appointmentTimeOfDay(appointment.startsAt)}
+                </li>
+            ))}
+        </ol>
+    </div>;
