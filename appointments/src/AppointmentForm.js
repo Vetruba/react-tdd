f7ca91ded23f46b85e37b474cccadf7efc5d106b
Chase Vetruba <Vetruba00@gmail.com>
2024-04-09 15:26:28 -0500

    before Making a choice from radio buttons

---
+++ appointments/src/AppointmentForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+export const AppointmentForm = ({
+  selectableServices,
+  service,
+  onSubmit
+}) => {
+  const [appointment, setAppointment] = useState({ service });
+  const handleChange = ({ target: { value } }) =>
+    setAppointment(appointment => ({
+      ...appointment,
+      service: value
+    }));
+
+  return <form id="appointment" onSubmit={() => onSubmit(appointment)}>
+    <label htmlFor='service'>Salon service</label>
+    <select
+      name="service"
+      id="service"
+      value={service}
+      onChange={handleChange}>
+
+      <option />
+      {selectableServices.map(s => (
+        <option key={s}>{s}</option>
+      ))}
+    </select>
+  </form>
+};
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cut',
+    'Blow-dry',
+    'Cut & color',
+    'Beard trim',
+    'Cut & beard trim',
+    'Extensions']
+};
